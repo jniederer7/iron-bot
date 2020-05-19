1b5df75da9adf35c246247c2f974e8fee70b696e
justin niederer <jniederer7@gmail.com>
2020-05-19 13:52:24 -0400

    added view engine to bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,8 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
 	res.render('index');
 })
